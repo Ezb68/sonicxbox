ca1fcfe14cc5e24a4096d5449bfc069d1f4bef12
coco <coco50511@outlook.com>
2019-12-26 07:42:04 +0800

    sample code for sol compiler v0.4.0 has been added

---
+++ test/fixtures/metacoin-box-0.4.0/sonicxbox.js
@@ -0,0 +1,36 @@
+module.exports = {
+  networks: {
+    mainnet: {
+      // Don't put your private key here:
+      // privateKey: process.env.PRIVATE_KEY_MAINNET,
+      /*
+Create a .env file (it must be gitignored) containing something like
+
+  export PRIVATE_KEY_MAINNET=D8B708BFFFA424473D83349CF4C6A2395E4436E065B60F0BF31E582281256D1C
+
+Then, run the migration with:
+
+  source .env && sonicxbox migrate --network mainnet
+
+*/
+      userFeePercentage: 100,
+      feeLimit: 1e8,
+	  fullNode: "https://fullnode.sonicxhub.com",
+      solidityNode: "https://solnode.sonicxhub.com",
+      eventServer: "https://event.sonicxhub.com/",
+	  fullHost: "https://fullnode.sonicxhub.com",
+      network_id: "1"
+    },
+    development: {
+      privateKey: '960951D34032289501B99292B110CFC337D36EE1100CD1D4C30B0AAB7B0E9474',
+      userFeePercentage: 100,
+      feeLimit: 1e8,
+	  fullNode: "http://127.0.0.1:8190",
+      solidityNode: "http://127.0.0.1:8191",
+      eventServer: "http://127.0.0.1:8080",
+	  fullHost: "http://127.0.0.1:8190",
+      network_id: "1"
+    },
+	useZeroFourCompiler: true
+  }
+}
